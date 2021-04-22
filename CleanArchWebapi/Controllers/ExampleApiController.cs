84e9aa10d867f14d79fbd9e5ac91c7b18dfc4893
nehaaror <72908070+nehaaror@users.noreply.github.com>
2021-04-23 02:16:03 +0530

    adding the solution and first method

---
+++ CleanArchWebapi/Controllers/ExampleApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CleanArchWebapi.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        public string Get()
+        {
+            return "My First Api Example";
+        }
+    }
+}
